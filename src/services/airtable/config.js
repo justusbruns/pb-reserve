e95bc5f359c6f286d328ed039827774179ba6ff2
Justus Bruns <justusbruns@Justuss-MacBook-Pro.local>
2024-11-18 09:58:28 +0100

    airtable 2 test integration

--- src/services/airtable/config.js
+++ src/services/airtable/config.js
@@ -1,7 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 exports.__esModule = true;
 exports.base = void 0;
-var airtable_1 = require("airtable");
+var airtable_1 = __importDefault(require("airtable"));
 // Configure Airtable with Personal Access Token
 var base = new airtable_1["default"]({
     apiKey: import.meta.env.VITE_AIRTABLE_PAT,
